3bee43b9132a39e2f23717259bf0f61a51459142
lcardena9 <42814907+lcardena9@users.noreply.github.com>
2019-06-04 17:21:24 -0700

    first upload, landing page

---
+++ precache-manifest.72b96748eb139ac9098d2c6d4406e4a9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee04f2380808e82a764106f9a9e9b672",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fbdd78fb203c670dcdd4",
+    "url": "/static/css/main.dce52802.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "fbdd78fb203c670dcdd4",
+    "url": "/static/js/main.2df892d6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "63d022bdac98ef54e42135eb7e170afe",
+    "url": "/static/media/duncecaplogo.63d022bd.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
